eb4b61b7cede94c73aac9f939a1dd94d7a6b32f8
Jesse Vernallis <92563135+jesseVernallis@users.noreply.github.com>
2024-04-06 12:44:07 -0700

    challenge 1 done

---
+++ challenge1Client3.py
@@ -0,0 +1,88 @@
+import os
+import json
+import random
+from dotenv import load_dotenv
+
+import paho.mqtt.client as paho
+from paho import mqtt
+import time
+
+
+# setting callbacks for different events to see if it works, print the message etc.
+def on_connect(client, userdata, flags, rc, properties=None):
+    """
+        Prints the result of the connection with a reasoncode to stdout ( used as callback for connect )
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param flags: these are response flags sent by the broker
+        :param rc: stands for reasonCode, which is a code for the connection result
+        :param properties: can be used in MQTTv5, but is optional
+    """
+    print("CONNACK received with code %s." % rc)
+
+
+# with this callback you can see if your publish was successful
+def on_publish(client, userdata, mid, properties=None):
+    """
+        Prints mid to stdout to reassure a successful publish ( used as callback for publish )
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param mid: variable returned from the corresponding publish() call, to allow outgoing messages to be tracked
+        :param properties: can be used in MQTTv5, but is optional
+    """
+    print("mid: " + str(mid))
+
+
+# print which topic was subscribed to
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    """
+        Prints a reassurance for successfully subscribing
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param mid: variable returned from the corresponding publish() call, to allow outgoing messages to be tracked
+        :param granted_qos: this is the qos that you declare when subscribing, use the same one for publishing
+        :param properties: can be used in MQTTv5, but is optional
+    """
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg):
+    """
+        Prints a mqtt message to stdout ( used as callback for subscribe )
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param msg: the message with topic and payload
+    """
+
+    print("message: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+
+if __name__ == '__main__':
+
+    broker_address = "9f3415ad498b4edf9277085bae075b5d.s1.eu.hivemq.cloud"
+    broker_port = 8883
+    username = "Player_1"
+    password = "Player_1"
+
+    client = paho.Client(callback_api_version=paho.CallbackAPIVersion.VERSION1, client_id="client", userdata=None, protocol=paho.MQTTv5)
+    
+    # enable TLS for secure connection
+    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    # set username and password
+    client.username_pw_set(username, password)
+    # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+    client.connect(broker_address, broker_port)
+
+    # setting callbacks, use separate functions like above for better visibility
+    client.on_subscribe = on_subscribe # Can comment out to not print when subscribing to new topics
+    client.on_message = on_message
+    client.on_publish = on_publish # Can comment out to not print when publishing to topics
+
+    client.subscribe("topic1")
+    client.subscribe("topic2")
+    client.loop_forever()
+    
+
+    
+    
